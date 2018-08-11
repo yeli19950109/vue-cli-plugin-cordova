46dbc78c13d138461587ebba29f0532918acd31c
yel <yel@sucsoft.com>
2018-08-11 15:43:56 +0800

    修改配置

--- generator/index.js
+++ generator/index.js
@@ -1,107 +1,64 @@
 module.exports = (api, options, rootOptions) => {
-  const fs = require('fs')
+    const fs = require('file-system');
 
-  api.extendPackage({
-    scripts: {
-      'cordova-serve': 'vue-cli-service cordova-serve',
-      'cordova-build': 'vue-cli-service build --dest www'
-    },
-    dependencies: {
-      'babel-polyfill': '^6.26.0',
-      'cordova-android': '^7.1.0',
-      'cordova-browser': '^5.0.3',
-      'cordova-ios': '^4.5.4',
-      'cordova-plugin-device': '^2.0.1',
-      'cordova-plugin-ionic-webview': '^1.2.0',
-      'cordova-plugin-splashscreen': '^5.0.2',
-      'cordova-plugin-statusbar': '^2.4.2',
-      'cordova-plugin-whitelist': '^1.3.3',
-      'material-design-icons': '^3.0.1',
-      'typeface-roboto': '0.0.54',
-      'vuetify': '^1.0.14'
-    },
-    cordova: {
-      plugins: {
-        'cordova-plugin-device': {},
-        'cordova-plugin-ionic-webview': {},
-        'cordova-plugin-splashscreen': {},
-        'cordova-plugin-statusbar': {},
-        'cordova-plugin-whitelist': {}
-      },
-      platforms: [
-        'android',
-        'browser',
-        'ios'
-      ]
-    }
-  })
-
-  const hasTS = api.hasPlugin('typescript')
-  const routerPath = api.resolve(`./src/router.${hasTS ? 'ts' : 'js'}`)
-  const hasRouter = fs.existsSync(routerPath)
-  api.render('./templates', { hasTS, hasRouter })
-
-  api.postProcessFiles(files => {
-    // index.html
-    const indexHtml = files['public/index.html']
-    if (indexHtml) {
-      const lines = indexHtml.split(/\r?\n/g).reverse()
-      const lastMetaIndex = lines.findIndex(line => line.match(/\s+<meta/))
-      lines[lastMetaIndex] +=
-        `\n    <!-- TODO: You should modify CSP for production build -->` +
-        `\n    <meta http-equiv="Content-Security-Policy" content="default-src gap: data: 'unsafe-inline' 'unsafe-eval' *">`
-      files['public/index.html'] = lines.reverse().join('\n')
-    }
-    // main.js
-    const isTS = 'src/main.ts' in files
-    const mainFile = `src/main.${isTS ? 'ts' : 'js'}`
-    const main = files[mainFile]
-    if (main) {
-      const lines = main.split(/\r?\n/g).reverse()
-      const topIndex = lines.length - 1
-      lines[topIndex] =
-        `import 'typeface-roboto';\n` +
-        `import 'material-design-icons/iconfont/material-icons.css';\n` +
-        `import 'vuetify/dist/vuetify.css';\n` +
-        `import 'babel-polyfill';\n` +
-        lines[topIndex]
-      const lastImportIndex = lines.findIndex(line => line.match(/^import/))
-      lines[lastImportIndex] +=
-        `\nimport cordovaLoader from './cordovaLoader';` +
-        `\nimport vuetify from 'vuetify';`
-      const startAt = lines[0] === '' ? 1 : 0
-      const declareVueIndex = lines.findIndex(line => line.match(/new Vue/))
-      for (let i = startAt; i <= declareVueIndex; i++) {
-        if (i === startAt) {
-          lines[i] = `  ${lines[i]}\n});`
-        } else if (i === declareVueIndex) {
-          lines[i] = `Vue.use(vuetify);\n\ncordovaLoader(() => {\n  ${lines[i]}`
-        } else {
-          lines[i] = `  ${lines[i]}`
+    api.extendPackage({
+        scripts: {
+            'cordova-dev': 'vue-cli-service build --mode development --dest www',
+            'cordova-prod': 'vue-cli-service build --dest www',
+        },
+        dependencies: {
+            'cordova-android': '^7.1.1',
+            'cordova-ios': '^4.5.5',
+            'cordova-plugin-device': '^2.0.2',
+            'cordova-plugin-splashscreen': '^5.0.2',
+            'cordova-plugin-statusbar': '^2.4.2',
+            'cordova-plugin-whitelist': '^1.3.3',
+            "cordova-plugin-ionic-keyboard": "^2.1.2"
+        },
+        cordova: {
+            plugins: {
+                "cordova-plugin-whitelist": {},
+                "cordova-plugin-device": {},
+                "cordova-plugin-splashscreen": {},
+                "cordova-plugin-ionic-keyboard": {},
+                "cordova-plugin-statusbar": {}
+            },
+            platforms: [
+                'android',
+                'ios'
+            ]
         }
-      }
-      files[mainFile] = lines.reverse().join('\n')
-    }
-    // cordovaLoarder.js
-    if (isTS) {
-      const loader = 'src/cordovaLoader'
-      const content = files[`${loader}.js`]
-      files[`${loader}.ts`] = content
-      delete files[`${loader}.js`]
-    }
-  })
+    });
 
-  api.onCreateComplete(() => {
-    // .gitignore - not included in files on postProcessFiles
-    const ignorePath = api.resolve('.gitignore')
-    const ignore = fs.existsSync(ignorePath)
-      ? fs.readFileSync(ignorePath, 'utf-8')
-      : ''
-    fs.writeFileSync(ignorePath, ignore + '\n# Cordova\n/www\n/platforms\n/plugins\n')
-
-    // create symlinks
-    fs.symlinkSync('../platforms', './public/cordova', 'dir')
-    fs.symlinkSync('../platforms/browser/www/config.xml', './public/config.xml')
-  })
-}
+    api.onCreateComplete(() => {
+        // .gitignore - not included in files on postProcessFiles
+        const ignorePath = api.resolve('.gitignore');
+        const ignore = fs.existsSync(ignorePath)
+            ? fs.readFileSync(ignorePath, 'utf-8')
+            : '';
+        fs.writeFileSync(ignorePath, ignore + '\n# Cordova\n/www\n/platforms\n/plugins\n');
+        fs.writeFileSync(
+            api.resolve(".browserslistrc"),
+            "ios > 9\n" +
+            "android > 4.4.4\n"
+        );
+        fs.writeFileSync(
+            api.resolve(".editorconfig"),
+            "# http://editorconfig.org\n" +
+            "root = true\n" +
+            "\n" +
+            "[*]\n" +
+            "charset = utf-8\n" +
+            "indent_style = tab\n" +
+            "end_of_line = lf\n" +
+            "insert_final_newline = true\n" +
+            "trim_trailing_whitespace = true\n" +
+            "\n" +
+            "[*.md]\n" +
+            "insert_final_newline = false\n" +
+            "trim_trailing_whitespace = false"
+        )
+        api.render('./template', options)
+    })
+};
 
--- index.js
+++ index.js
@@ -1,93 +1,53 @@
 const {
-  info
-} = require('@vue/cli-shared-utils')
-
-const defaults = {
-  mode: 'development',
-  host: '0.0.0.0',
-  port: 8080,
-  https: false
-}
+    info
+} = require('@vue/cli-shared-utils');
 
 module.exports = (api, options) => {
-  const address = require('address')
-  const portfinder = require('portfinder')
+    api.chainWebpack(webpackConfig => {
+        if (process.env.NODE_ENV === 'production') {
+            webpackConfig.plugin('copy')
+                .tap(args => {
+                    args[0][0].ignore.push('cordova');
+                    args[0][0].ignore.push('config.xml');
+                    return args
+                });
+            webpackConfig.plugin('cordova')
+                .use(require('html-webpack-include-assets-plugin'), [{
+                    assets: 'cordova.js',
+                    append: false,
+                    publicPath: false
+                }]);
+            // FIXME: This is a temporary patch.
+            // When the following PR is merged into file-loader, modify it to use cssOutputPath and useRelativePath.
+            // https://github.com/webpack-contrib/file-loader/pull/150
+            webpackConfig.plugin('extract-css')
+                .tap(args => {
+                    args[0].filename = '[name].[contenthash:8].css';
+                    args[0].chunkFilename = '[name].[id].[contenthash:8].css';
+                })
+        }
+    });
 
-  api.registerCommand('cordova-serve', {
-    description: 'start development server for cordova',
-    usage: 'vue-cli-service cordova-serve [options]',
-    options: {
-      '--open': `open browser on server start`,
-      '--host': `specify host (default: ${defaults.host})`,
-      '--port': `specify port (default: ${defaults.port})`,
-      '--https': `use https (default: ${defaults.https})`
-    }
-  }, args => {
-    const projectDevServerOptions = options.devServer || {}
-    portfinder.basePort = args.port || process.env.PORT || projectDevServerOptions.port || defaults.port
-    return portfinder.getPortPromise().then(port => {
-      const serveArgs = {
-        open: args.open,
-        host: args.host || process.env.HOST || projectDevServerOptions.host || defaults.host,
-        port,
-        https: args.https || projectDevServerOptions.https || defaults.https,
-        lanIp: address.ip()
-      }
-      const wwwDirPath = api.resolve('www')
-      info('your www/index.html is overwrited.')
-      copyRedirectHtml(serveArgs, wwwDirPath)
-      return api.service.run('serve', serveArgs)
+    api.configureWebpack(config => {
+        if (process.env.NODE_ENV === 'production') {
+            // Default publicPath is '/'
+            // And it's not working well with the 'file://' protocol
+            config.output.publicPath = ''
+        }
+        if (process.env.NODE_ENV === 'development') {
+            // Default publicPath is '/'
+            // And it's not working well with the 'file://' protocol
+            config.devtool = 'cheap-eval-source-map';
+        }
+    });
+
+    api.configureDevServer((config) => {
+        config.before = (app) => {
+            app.get('/cordova.js', function (req, res) {
+                res.setHeader('Content-Type', 'application/javascript');
+                res.write('');
+                res.end('');
+            });
+        };
     })
-  })
-
-  api.chainWebpack(webpackConfig => {
-    if (process.env.NODE_ENV === 'production') {
-      webpackConfig.plugin('copy')
-        .tap(args => {
-          args[0][0].ignore.push('cordova')
-          args[0][0].ignore.push('config.xml')
-          return args
-        })
-      webpackConfig.plugin('cordova')
-        .use(require('html-webpack-include-assets-plugin'), [{
-          assets: 'cordova.js',
-          append: false,
-          publicPath: false
-        }])
-      // FIXME: This is a temporary patch.
-      // When the following PR is merged into file-loader, modify it to use cssOutputPath and useRelativePath.
-      // https://github.com/webpack-contrib/file-loader/pull/150
-      webpackConfig.plugin('extract-css')
-        .tap(args => {
-          args[0].filename = '[name].[contenthash:8].css',
-          args[0].chunkFilename = '[name].[id].[contenthash:8].css'
-        })
-    }
-  })
-
-  api.configureWebpack(config => {
-    if (process.env.NODE_ENV === 'production') {
-      // Default publicPath is '/'
-      // And it's not working well with the 'file://' protocol
-      config.output.publicPath = ''
-    }
-  })
-}
-
-function copyRedirectHtml (args, distDirPath) {
-  const fs = require('fs')
-  const path = require('path')
-  const ejs = require('ejs')
-  const templatePath = path.resolve(__dirname, './redirect.ejs')
-  const templateStr = fs.readFileSync(templatePath, 'utf-8')
-  const htmlStr = ejs.render(templateStr, args)
-  if (!fs.existsSync(distDirPath)) {
-    fs.mkdirSync(distDirPath)
-  }
-  const distPath = path.resolve(distDirPath, 'index.html')
-  fs.writeFileSync(distPath, htmlStr)
-}
-
-module.exports.defaultModes = {
-  'cordova-serve': 'development'
-}
+};
